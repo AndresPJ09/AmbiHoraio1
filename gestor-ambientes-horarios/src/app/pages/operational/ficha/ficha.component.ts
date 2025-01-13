c305848a5a71723e9d66fd86d8267e2a4647c78d
AndresPJ09 <carlosandrespantojaramillo@gmail.com>
2025-01-13 17:31:35 -0500

    Cambios en ficha

--- gestor-ambientes-horarios/src/app/pages/operational/ficha/ficha.component.ts
+++ gestor-ambientes-horarios/src/app/pages/operational/ficha/ficha.component.ts
@@ -29,15 +29,21 @@ export class FichaComponent implements OnInit {
     num_semanas: '', state: true };
   users: any[] = [];
   programas: any[] = [];
+  ambientes: any[] = [];
+  proyectos: any[] = [];
   isModalOpen = false;
   filteredUsers: any[] = [];
   filteredProgramas: any[] = [];
+  filteredAmbientes: any[] = [];
+  filteredProyectos: any[] = [];
   isDropdownOpen = false;
   isEditing = false;
 
   private apiUrl = 'http://localhost:5062/api/Ficha';
   private usersUrl = 'http://localhost:5062/api/User';
   private programasUrl = 'http://localhost:5062/api/Programa';
+  private ambientesUrl = 'http://localhost:5062/api/Ambiente';
+  private proyectosUrl = 'http://localhost:5062/api/Proyecto';
 
   constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
 
@@ -45,6 +51,8 @@ export class FichaComponent implements OnInit {
     this.getFichas();
     this.getUsers();
     this.getProgramas();
+    this.getAmbientes();
+    this.getProyectos();
   }
 
   getFichas(): void {
@@ -76,9 +84,28 @@ export class FichaComponent implements OnInit {
     );
   }
 
-  getUserNombre(userId: number): string {
-    const user = this.users.find(use => use.id === userId);
-    return user ? user.username : 'Desconocido';
+  getAmbientes(): void {
+    this.http.get<any[]>(this.ambientesUrl).subscribe(
+      (ambientes) => {
+        this.ambientes = ambientes;
+        this.filteredAmbientes = ambientes;
+      },
+      (error) => {
+        console.error('Error fetching ambientes:', error);
+      }
+    );
+  }
+
+  getProyectos(): void {
+    this.http.get<any[]>(this.proyectosUrl).subscribe(
+      (proyectos) => {
+        this.proyectos = proyectos;
+        this.filteredProyectos = proyectos;
+      },
+      (error) => {
+        console.error('Error fetching proyectos:', error);
+      }
+    );
   }
 
   getProgramas(): void {
@@ -106,6 +133,28 @@ export class FichaComponent implements OnInit {
     }
   }
 
+  searchAmbiente(event: any): void {
+    const term = event.target.value.toLowerCase();
+    if (!term) {
+      this.filteredAmbientes = this.ambientes;
+    } else {
+      this.filteredAmbientes = this.ambientes.filter(ambiente =>
+        ambiente.nombre.toLowerCase().includes(term)
+      );
+    }
+  }
+
+  searchProyecto(event: any): void {
+    const term = event.target.value.toLowerCase();
+    if (!term) {
+      this.filteredProyectos = this.proyectos;
+    } else {
+      this.filteredProyectos = this.proyectos.filter(proyecto =>
+        proyecto.nombre.toLowerCase().includes(term)
+      );
+    }
+  }
+
   onProgramaSelect(event: any): void {
     const selectedprograma = this.programas.find(programa =>
       programa.nombre === event.option.value
@@ -117,9 +166,46 @@ export class FichaComponent implements OnInit {
     }
   }
 
+  onAmbienteSelect(event: any): void {
+    const selectedambiente = this.ambientes.find(ambiente =>
+      ambiente.nombre === event.option.value
+    );
+    if (selectedambiente) {
+      this.ficha.ambienteId = selectedambiente.id;
+      this.ficha.ambienteNombre = selectedambiente.nombre;
+      this.filteredAmbientes = [];
+    }
+  }
+
+  onProyectoSelect(event: any): void {
+    const selectedproyecto = this.proyectos.find(proyecto =>
+      proyecto.nombre === event.option.value
+    );
+    if (selectedproyecto) {
+      this.ficha.proyectoId = selectedproyecto.id;
+      this.ficha.proyectoNombre = selectedproyecto.nombre;
+      this.filteredProyectos = [];
+    }
+  }
+
   getProgramaNombre(programaId: number): string {
     const programa = this.programas.find(progr => progr.id === programaId);
     return programa ? programa.nombre : 'Desconocido';
+  }
+
+  getUserNombre(userId: number): string {
+    const user = this.users.find(use => use.id === userId);
+    return user ? user.username : 'Desconocido';
+  }
+
+  getAmbienteNombre(ambienteId: number): string {
+    const ambiente = this.ambientes.find(ambi => ambi.id === ambienteId);
+    return ambiente ? ambiente.nombre : 'Desconocido';
+  }
+
+  getProyectoNombre(proyectoId: number): string {
+    const proyecto = this.proyectos.find(use => use.id === proyectoId);
+    return proyecto ? proyecto.nombre : 'Desconocido';
   }
 
   openModal(): void {
@@ -131,6 +217,9 @@ export class FichaComponent implements OnInit {
     this.resetForm();
     this.isEditing = false;
     this.filteredUsers = [];
+    this.filteredAmbientes = [];
+    this.filteredProyectos = [];
+    this.filteredProgramas = [];
   }
 
   onSubmit(form: NgForm): void {
@@ -169,6 +258,16 @@ export class FichaComponent implements OnInit {
     const selectedprograma = this.programas.find(progr => progr.id === this.ficha.programaId);
     if (selectedprograma) {
       this.ficha.programaNombre = selectedprograma.nombre;
+    }
+
+    const selectedambiente = this.ambientes.find(progr => progr.id === this.ficha.ambienteId);
+    if (selectedambiente) {
+      this.ficha.ambienteNombre = selectedambiente.nombre;
+    }
+
+    const selectedproyecto = this.proyectos.find(progr => progr.id === this.ficha.proyectoId);
+    if (selectedproyecto) {
+      this.ficha.proyectoNombre = selectedproyecto.nombre;
     }
     this.isEditing = true;
     this.openModal();
@@ -209,5 +308,8 @@ export class FichaComponent implements OnInit {
       fin_lectiva: new Date().toISOString().slice(0, 10), 
       num_semanas: '', state: true };
     this.filteredUsers = [];
+    this.filteredAmbientes = [];
+    this.filteredProyectos = [];
+    this.filteredProgramas = [];
   }
 }
